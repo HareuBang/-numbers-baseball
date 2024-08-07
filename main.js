9bfaa2de3053f52e0d4cc048d564a29da2a681ec
HareuBang <162522895+HareuBang@users.noreply.github.com>
2024-08-08 00:48:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const $input = document.querySelector('input');
 const $button = document.querySelector('button');
-const $span = document.querySelector('span');
+const $p = document.querySelector('p');
 
-let computer = fourDigitNumber();
+let computer = generateRandomNumber();
+let count = 0;
 
 // 컴퓨터 숫자 4자리 설정
-function fourDigitNumber() {
+function generateRandomNumber() {
   let randomNumbers = [];
 
   while(randomNumbers.length < 4){
     let num = Math.floor(Math.random() * 10);
 
+    // 중복 숫자 판별
     if(!randomNumbers.includes(num))
       randomNumbers.push(num);
   }
@@ -25,27 +27,52 @@ $button.addEventListener('click', () => {
   let player = inputValidation($input.value);
 
   if(player) {
-    let [ strike, ball, out ] = [...guessNumber(player)]
+    let [ strike, ...rest ] = [...guessNumber(player)]
     
     if(strike === 4){
-      $span.innerHTML += "!! Home run !! <br/>게임을 다시 시작합니다."
-
-      setTimeout(() => {
-        resetGame();
-      }, 1500)
+      resetGame("Home Run");
     } else {
-      $span.innerHTML += `inputNumber: ${player} / Strike: ${strike} Ball: ${ball} / Out: ${out} // computer ${computer}<br/>`
+      outputResult(player, [strike, ...rest]);  
     }
 
   } else {
-    alert("중복되지 않는 4자리 숫자를 입력해주세요.");
+    alert("중복되지 않는 네 자리 숫자를 입력해주세요.");
     $input.value = "";
     $input.focus();
   }
 })
 
+// 결과 출력
+function outputResult(player, [strike, ball, out]) {
+  if(count <= 10){
+    ++count;
+    $input.value = ""
+    $p.innerHTML += 
+    `
+    <table>
+      <tr>
+        <th rowspan="2">${count}</th>
+        <th>Player</th>
+        <th style="color:yellow">Strike</th>
+        <th style="color:green">Ball</th>
+        <th style="color:red">Out</th>
+      </tr>
+      <tr>
+        <td>${player.join('')}</td>
+        <td>${strike}</td>
+        <td>${ball}</td>
+        <td>${out}</td>
+      </tr>
+    </table>
+    `
+    if(count === 10)
+      resetGame("Computer Win");
+  }
+}
+
 // 입력 값 검사
 function inputValidation(inputValue) {
+  // new Set() 중복 제거 - split().filter() 공백 제거 - 숫자 타입 변환 map()
   let inputList = [...new Set(inputValue.split('').filter(numStr => numStr !== " "))].map(Number);
   
   return inputList.length === 4 && inputList.every(num => 0 <= num && num <= 9)
@@ -71,9 +98,14 @@ function guessNumber(player) {
   return result;
 }
 
-function resetGame() {
-  $input.value = "";
-  $input.focus();
-  $span.innerHTML = "";
-  computer = fourDigitNumber();
+// 게임 초기화
+function resetGame(message) {
+  $p.innerHTML += `!! ${message} !! <br/>게임을 다시 시작합니다.`
+  setTimeout(() => {
+    $input.value = "";
+    $input.focus();
+    $p.innerHTML = "";
+    computer = generateRandomNumber();
+    count = 0;
+  }, 1500)
 }
